ca54927c71fe1d5abad3d9cbc371d15ae6d45ec6
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2023-02-16 10:56:19 +0800

    Docs

--- RMTools/RMTools_1D/__init__.py
+++ RMTools/RMTools_1D/__init__.py
@@ -1,12 +1,10 @@
 """RMTools in 1D"""
 from . import (
-    calculate_RMSF,
+    calc_rmsf,
     clean_model,
-    do_QUfit_1D_mnest,
-    do_RMclean_1D,
-    do_RMsynth_1D_fromFITS,
-    do_RMsynth_1D,
-    mk_test_ascii_data,
-    rmtools_bwdepol,
-    rmtools_bwpredict,
+    qufit,
+    rmclean,
+    rmsynth,
+    bwdepol,
+    bwpredict,
 )
