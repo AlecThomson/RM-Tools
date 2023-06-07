aaef903a1658e222cdbd806520d8177035ece577
Thomson, Alec (CASS, Kensington) <alec.thomson@csiro.au>
2023-06-07 16:06:29 +1000

    Formatting

--- RMtools_3D/assemble_chunks.py
+++ RMtools_3D/assemble_chunks.py
@@ -9,47 +9,63 @@ output files from 3D RM synthesis back into larger cubes.
 @author: cvaneck
 """
 
-import numpy as np
 import argparse
-from astropy.io import fits
-import os.path as path
-from math import ceil, floor, log10
-from glob import glob
-import re
-from tqdm.asyncio import tqdm, trange
 import asyncio
+import os.path as path
+import re
 from datetime import datetime
+from glob import glob
+from math import ceil, floor, log10
+from typing import Coroutine, List, Tuple
+
+import numpy as np
+from astropy.io import fits
+from tqdm.asyncio import tqdm, trange
 
 
 def main():
-    """This function will assemble a large FITS file or cube from smaller chunks.
-    """
+    """This function will assemble a large FITS file or cube from smaller chunks."""
 
-    descStr="""
+    descStr = """
     Assemble a FITS image/cube from small pieces. The new image will be created
     in the running directory.
     Supply one of the chunk files (other files will be identified by name pattern).
     Output name will follow the name of the input chunk, minus the '.C??.'
     """
 
-    parser = argparse.ArgumentParser(description=descStr,
-                             formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("chunkname", metavar="chunk.fits",
-                        help="One of the chunks to be assembled")
-    parser.add_argument("-f", dest="output", default=None,
-                        help="Specify output file name [basename of chunk]")
-    parser.add_argument("-o", dest="overwrite", action="store_true",
-                        help="Overwrite existing file? [False].")
+    parser = argparse.ArgumentParser(
+        description=descStr, formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument(
+        "chunkname", metavar="chunk.fits", help="One of the chunks to be assembled"
+    )
+    parser.add_argument(
+        "-f",
+        dest="output",
+        default=None,
+        help="Specify output file name [basename of chunk]",
+    )
+    parser.add_argument(
+        "-o",
+        dest="overwrite",
+        action="store_true",
+        help="Overwrite existing file? [False].",
+    )
 
     args = parser.parse_args()
 
     if args.output == None:
-        output_filename='.'.join([ x for x in args.chunkname.split('.') if not
-                              (x.startswith('C') and x[1:].isnumeric()) ])
+        output_filename = ".".join(
+            [
+                x
+                for x in args.chunkname.split(".")
+                if not (x.startswith("C") and x[1:].isnumeric())
+            ]
+        )
     else:
-        output_filename=args.output
+        output_filename = args.output
 
-    asyncio.run( 
+    asyncio.run(
         assemble(
             chunkname=args.chunkname,
             output_filename=output_filename,
@@ -57,69 +73,85 @@ def main():
         )
     )
 
-async def worker(i,chunk,large_hdul,startx, stopx, starty, stopy):
+
+async def worker(
+    i: int,
+    chunk: np.ndarray,
+    large_hdul: fits.HDUList,
+    startx: int,
+    stopx: int,
+    starty: int,
+    stopy: int,
+) -> Coroutine[None]:
     print(f"{datetime.utcnow()} Worker {i} starting...")
-    # large_hdul = await asyncio.to_thread(_worker, large_hdul, chunk, startx, stopx, starty, stopy)
-    await asyncio.to_thread(update_and_write, large_hdul, chunk, startx, stopx, starty, stopy)
-    # await asyncio.sleep(1)
-    # await asyncio.to_thread(large_hdul[0].data[...,starty:stopy,startx:stopx].__setitem__, slice(None), chunk)
-    # await asyncio.to_thread(large_hdul.flush,verbose=True)
+    await asyncio.to_thread(
+        update_and_write, large_hdul, chunk, startx, stopx, starty, stopy
+    )
     print(f"{datetime.utcnow()} Worker {i} done")
+
 
 def update_and_write(large_hdul, chunk, startx, stopx, starty, stopy):
     large_hdul[0].data[
-            ...,
-            starty:stopy,
-            startx:stopx,
+        ...,
+        starty:stopy,
+        startx:stopx,
     ] = chunk
     large_hdul.flush(verbose=True)
+
 
 async def assemble(
     chunkname: str,
     output_filename: str,
     overwrite: bool = False,
 ):
-    #Get all the chunk filenames. Missing chunks will break things!
-    filename=re.search('\.C\d+\.',chunkname)
-    chunkfiles=glob(chunkname[0:filename.start()]+'.C*.'+chunkname[filename.end():])
+    # Get all the chunk filenames. Missing chunks will break things!
+    filename = re.search("\.C\d+\.", chunkname)
+    chunkfiles = glob(
+        chunkname[0 : filename.start()] + ".C*." + chunkname[filename.end() :]
+    )
     chunkfiles.sort()
 
-
-    old_header=fits.getheader(chunkfiles[0])
-    x_dim=old_header['OLDXDIM']
-    y_dim=old_header['OLDYDIM']
-    Nperchunk=old_header['NAXIS1']
-    Npix_image=x_dim*y_dim
-    num_chunks=ceil(Npix_image/Nperchunk)
-    Ndim=old_header['NAXIS']
+    old_header = fits.getheader(chunkfiles[0])
+    x_dim = old_header["OLDXDIM"]
+    y_dim = old_header["OLDYDIM"]
+    Nperchunk = old_header["NAXIS1"]
+    Npix_image = x_dim * y_dim
+    num_chunks = ceil(Npix_image / Nperchunk)
+    Ndim = old_header["NAXIS"]
 
     if (Ndim != 4) and (Ndim != 3) and (Ndim != 2):
-        raise Exception('Right now this code only supports FITS files with 2-4 dimensions!')
+        raise Exception(
+            "Right now this code only supports FITS files with 2-4 dimensions!"
+        )
 
-    new_header=old_header.copy()
-    del new_header['OLDXDIM']
-    del new_header['OLDYDIM']
-    new_header['NAXIS1']=x_dim
-    new_header['NAXIS2']=y_dim
+    new_header = old_header.copy()
+    del new_header["OLDXDIM"]
+    del new_header["OLDYDIM"]
+    new_header["NAXIS1"] = x_dim
+    new_header["NAXIS2"] = y_dim
 
+    # Create blank file:
+    new_header.tofile(output_filename, overwrite=overwrite)
 
-    #Create blank file:
-    new_header.tofile(output_filename,overwrite=overwrite)
-
-    #According to astropy, this is how to create a large file without needing it in memory:
-    shape = tuple(new_header['NAXIS{0}'.format(ii)] for ii in range(1, new_header['NAXIS']+1))
-    with open(output_filename, 'rb+') as fobj:
-        fobj.seek(len(new_header.tostring()) + (np.product(shape) * np.abs(new_header['BITPIX']//8)) - 1)
-        fobj.write(b'\0')
-
+    # According to astropy, this is how to create a large file without needing it in memory:
+    shape = tuple(
+        new_header["NAXIS{0}".format(ii)] for ii in range(1, new_header["NAXIS"] + 1)
+    )
+    with open(output_filename, "rb+") as fobj:
+        fobj.seek(
+            len(new_header.tostring())
+            + (np.product(shape) * np.abs(new_header["BITPIX"] // 8))
+            - 1
+        )
+        fobj.write(b"\0")
 
     chunks = []
     for f in chunkfiles:
-        chunks.append(fits.getdata(f,memmap=True))
+        chunks.append(fits.getdata(f, memmap=True))
 
     tasks = []
-    with fits.open(output_filename,mode='update',memmap=True) as large_hdul:
-        large=large_hdul[0]
+    with fits.open(output_filename, mode="update", memmap=True) as large_hdul:
+        large = large_hdul[0]
         large_data = large.data
         large_shape = large_data.shape
         print(f"{large_shape=}")
@@ -135,14 +167,9 @@ async def assemble(
             # Fill in the large array with the chunk data
             print(f"{starty}:{stopy}")
             print(f"{startx}:{stopx}")
-            # large_hdul[0].data[
-            #     ...,
-            #     starty:stopy,
-            #     startx:stopx,
-            # ] = chunk
-
-            # large_hdul.flush(verbose=True)
-            task = asyncio.create_task(worker(i, chunk,large_hdul, startx, stopx, starty, stopy))
+            task = asyncio.create_task(
+                worker(i, chunk, large_hdul, startx, stopx, starty, stopy)
+            )
             # Check if the chunk filled the x or y dimension
             x_full = stopx == large_shape[-1]
             y_full = stopy == large_shape[-2]
@@ -151,13 +178,9 @@ async def assemble(
             starty = 0 if y_full else stopy
 
             tasks.append(task)
-        
-    
+
         await tqdm.gather(*tasks)
-            
+
 
 if __name__ == "__main__":
     main()
-
-
-
